a47ae23a07f3216bf5aaf3cfd63f36c7761cfd57
Andrei <oranceanuandrei@gmail.com>
2024-03-10 23:13:46 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace shoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        ///  private void create()
+    private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.InitialImage = ((System.Drawing.Image)(resources.GetObject("player.InitialImage")));
+            this.player.Location = new System.Drawing.Point(113, 72);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(125, 83);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.Transparent;
+            this.player2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player2.BackgroundImage")));
+            this.player2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player2.Image = ((System.Drawing.Image)(resources.GetObject("player2.Image")));
+            this.player2.InitialImage = ((System.Drawing.Image)(resources.GetObject("player2.InitialImage")));
+            this.player2.Location = new System.Drawing.Point(599, 267);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(125, 83);
+            this.player2.TabIndex = 2;
+            this.player2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(503, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shoot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        PictureBox[] bullets1 = new PictureBox[10];
+        PictureBox[] bullets2 = new PictureBox[10];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            create();
+            player.BackgroundImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            player2.BackgroundImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+        }
+
+        private int i = 0;
+        private void create()
+        {
+            PictureBox bullet = new PictureBox()
+            {
+                Name = "bullet",
+                Size = new Size(10, 5),
+                BackColor = Color.Red,
+                Location = new Point(player.Location.X + 30, player.Location.Y + 30),
+            };
+            PictureBox bullet2 = new PictureBox()
+            {
+                Name = "bullet",
+                Size = new Size(10, 5),
+                BackColor = Color.Blue,
+                Location = new Point(player2.Location.X - 25, player2.Location.Y + 30),
+                Visible = true,
+            };
+            bullets1[i] = bullet;
+            bullets2[i] = bullet2;
+            this.Controls.Add(bullets1[i]);
+            this.Controls.Add(bullets2[i]);
+            i++;
+            if(i == 3)
+            {
+                i = 0;
+            }
+        }
+        private int c = 0;
+        private bool down = false, up = false,left = false, right = false,down2 = false, up2 = false, right2 = false, left2 = false;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            create();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                up2 = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                down2 = true;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                left2 = true;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                right2 = true;
+            }
+            //////////////////////////////////////////
+            else if (e.KeyCode == Keys.W)
+            {
+                up = true;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                down = true;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                left = true;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                right = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                up2 = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                down2 = false;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                left2 = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                right2 = false;
+            }
+            /////////////
+            else if (e.KeyCode == Keys.W)
+            {
+                up = false ;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                down = false;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+
+        }
+        Random r = new Random();
+        int x1 = 0, x2 = 0;
+        bool ok = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ok = true;
+            for(int i = 0; i < bullets1.Length; i++)
+            {
+                if (bullets1[i]!=null)
+                {
+                    if(bullets1[i].Left + 10 > player2.Left&&bullets1[i].Top+5>player2.Top&&bullets1[i].Top<player2.Top+player2.Height&&bullets1[i].Left + 10<player2.Left + 8 )
+                    {
+                        this.Controls.Remove(bullets1[i]);
+                        bullets1[i].Dispose();
+                        bullets2[i].Visible = false;
+                        x1++;
+                        label1.Text = x1.ToString();
+                        ok = false;
+                    }
+                    bullets1[i].Left += 7;
+                    if (bullets1[i].Left > 600)
+                    {
+                        this.Controls.Remove(bullets1[i]);
+                        bullets1[i].Dispose();
+                        bullets2[i].Visible = false;
+
+                    }
+                }
+                if (bullets2[i] != null)
+                {
+                    if (bullets2[i].Left < player.Left +125 && bullets1[i].Top + 5 > player2.Top && bullets1[i].Top < player2.Top + player2.Height && bullets2[i].Left>player.Left + 117)
+                    {
+                        this.Controls.Remove(bullets2[i]);
+                        bullets2[i].Dispose();
+                        bullets2[i].Visible = false;
+                        x2++;
+                        label2.Text = x2.ToString();
+                    }
+                    bullets2[i].Left -= 7;
+                    if (bullets2[i].Left <5)
+                    {
+                        bullets2[i].Visible = false;
+                        this.Controls.Remove(bullets2[i]);
+                        bullets2[i].Dispose();
+                          
+                    }
+                }
+
+            }
+            if(up == true)
+            {
+                player.Top -= 5;
+            }else if(down == true)
+            {
+                player.Top += 5;
+            }
+            else if (left == true)
+            {
+                player.Left -= 5;
+            }
+            else if (right == true)
+            {
+                player.Left += 5;
+            }
+
+
+            if (up2 == true)
+            {
+                player2.Top -= 5;
+            }
+            else if (down2 == true)
+            {
+                player2.Top += 5;
+            }
+            else if (left2 == true)
+            {
+                player2.Left -= 5;
+            }
+            else if (right2 == true)
+            {
+                player2.Left += 5;
+            }
+        }
+    }
+}
